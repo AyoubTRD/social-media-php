8893714e53fdfd696e90a1f6cd3de2f57481d031
Ayoub Taouarda <ayoubtrdone@gmail.com>
2020-06-07 18:45:43 +0100

    Added responsivness tag

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?></title>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
     <style>
