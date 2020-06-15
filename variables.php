480b22388b3a0ee49db7565a34bf7a83a453a49b
Ayoub Taouarda <ayoubtrdone@gmail.com>
2020-06-15 19:09:03 +0100

    removed redundant db conenction

--- variables.php
+++ variables.php
@@ -20,18 +20,18 @@ $db_password = "cgzDktvcMOvN8DThReR9";
 $db_name = "bsyz4zydsluotmekr0br";
 
 
-if (isset($_ENV["DB_HOST"])) {
-  $db_host = $_ENV["DB_HOST"];
-}
-if (isset($_ENV["DB_USER"])) {
-  $db_user = $_ENV["DB_USER"];
-}
-if (isset($_ENV["DB_PASSWORD"])) {
-  $db_password = $_ENV["DB_PASSWORD"];
-}
-if (isset($_ENV["DB_NAME"])) {
-  $db_name = $_ENV["DB_NAME"];
-}
+//if (isset($_ENV["DB_HOST"])) {
+//  $db_host = $_ENV["DB_HOST"];
+//}
+//if (isset($_ENV["DB_USER"])) {
+//  $db_user = $_ENV["DB_USER"];
+//}
+//if (isset($_ENV["DB_PASSWORD"])) {
+//  $db_password = $_ENV["DB_PASSWORD"];
+//}
+//if (isset($_ENV["DB_NAME"])) {
+//  $db_name = $_ENV["DB_NAME"];
+//}
 
 $connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 if (!$connection) {
