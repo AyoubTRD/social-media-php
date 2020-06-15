0f405260f1315c0f709fed3758b221cc37396fda
Ayoub Taouarda <ayoubtrdone@gmail.com>
2020-06-15 15:01:46 +0100

    using env vars for db connection

--- variables.php
+++ variables.php
@@ -14,7 +14,25 @@ if (isset($_ENV["NODE_SERVER"]) && $_ENV["NODE_SERVER"]) {
 $is_logged_in = 0;
 $user = array();
 
-$connection = mysqli_connect("localhost", "root", "", "helloworld");
+$db_host = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "helloworld";
+
+if (isset($_ENV["DB_HOST"])) {
+  $db_host = $_ENV["DB_HOST"];
+}
+if (isset($_ENV["DB_USER"])) {
+  $db_user = $_ENV["DB_USER"];
+}
+if (isset($_ENV["DB_PASSWORD"])) {
+  $db_password = $_ENV["DB_PASSWORD"];
+}
+if (isset($_ENV["DB_NAME"])) {
+  $db_name = $_ENV["DB_NAME"];
+}
+
+$connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 if (!$connection) {
   die("Failed to connect to database");
 }
